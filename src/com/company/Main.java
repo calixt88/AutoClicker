a05faee57b7ea8c20bd08e543bfe4c6dc9cf1860
calixt88 <Ilovecalculus11>
2021-09-23 23:36:17 -0500

    Created comments with agenda.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 import java.awt.event.InputEvent;
-
 import java.awt.*;
-import java.awt.event.InputEvent;
+
+/*
+
+An auto clicker that closely resembles a human and may be able to surpass some anticheat.
+This auto clicker is currently in development and will only be used for experimental purposes.
+
+I am currently a software engineering student at Milwuakee School of Engineering.
+I am using this autoclicker as a personal project to better my java skills for the work/intern enviorment.
+
+Feel free to use this auto clicker at your OWN RISK. Auto clickers are against many of the terms of service
+for most online games. It is an unfair advantage and I advise that you try to refrain from cheating.
+
+ */
+
 
 public class Main {
 
