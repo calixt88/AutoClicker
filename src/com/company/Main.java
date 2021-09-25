61ee39fa89369ab0bb9f8a0ae7298f532f0f0bc4
calixt88 <Ilovecalculus11>
2021-09-25 15:07:26 -0500

    Added a pause method, a getRandomInteger method, and a four loop for the clicks.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,24 @@
 package com.company;
 import java.awt.event.InputEvent;
 import java.awt.*;
-import java.util.concurrent.ThreadLocalRandom;
 
 /*
 
-An auto clicker that closely resembles a human and may be able to surpass some anticheat.
+An auto clicker that closely resembles a human's inconsistencies and may be able to surpass some anti-cheat.
 This auto clicker is currently in development and will only be used for experimental purposes.
 
-I am currently a software engineering student at Milwuakee School of Engineering.
-I am using this autoclicker as a personal project to better my java skills for the work/intern enviorment.
+The program runs for a certain number of clicks but can be changed longer by changing the CLICKS variable.
+There are random timed pauses in between the press and release for the mouse. There are also random timed
+pauses in between the clicks themselves. This is to help make the clicks more 'human' like.
+
+The current CLICK number of 2000 takes approximately 35-40 minutes. On average each click takes a little over
+1 second to complete. (60 cps) I chose 2000 clicks because I am experimenting with the online MMORPG Runescape.
+I am using the 40-minute intervals as "breaks" for a person. I WILL have commented code that can be entered to
+begin the loop again after an allotted break time.
+
 
 Feel free to use this auto clicker at your OWN RISK. Auto clickers are against many of the terms of service
-for most online games. It is an unfair advantage and I advise that you to try to refrain from cheating.
-
+for most online games. It is an unfair advantage, and I advise that you to try to refrain from cheating.
  */
 
 
@@ -25,23 +30,38 @@ public class Main {
         int button = InputEvent.BUTTON1_DOWN_MASK; // Init of click button
         Robot r = new Robot(); //Init of robot object
         r.mousePress(button); //Mouse press
-        int pressTime = getRandomInteger(100, 2000);
-        System.out.println(pressTime);
+        int pressTime = getRandomInteger(100, 600);
+        //System.out.println(pressTime);
         Thread.sleep(pressTime); // Pause for random number in milliseconds (pressTime)
         r.mouseRelease(button); // Release click button
-        int releaseTime = getRandomInteger(100,400);
-        System.out.println(releaseTime);
+        int releaseTime = getRandomInteger(100,600);
+        //System.out.println(releaseTime);
         Thread.sleep(releaseTime); // Pause for random number in milliseconds (releaseTime)
     }
 
-    public static void main(String[] args) throws AWTException, InterruptedException {
-        click();
-
-
+    //Pause Method
+    static void pause() throws InterruptedException {
+        int pause = getRandomInteger(200,500);
+        Thread.sleep(pause);
     }
 
+    //Random integer method
     public static int getRandomInteger(int maximum, int minimum) {
         return ((int) (Math.random() * (maximum - minimum))) + minimum;
+    }
+
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int CLICKS = 2000; //amount of clicks performed before the program ends
+
+        for(int i = 0; i < CLICKS; i++)
+        {
+            pause();
+            click();
+            //pause();  //Can add another pause for security
+            System.out.println("Number of clicks: " + i);
+        }
+
+
     }
 
 }
