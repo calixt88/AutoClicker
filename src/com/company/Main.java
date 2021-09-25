f7243e6479786a8c298e4a013824dfd60fa1ef46
calixt88 <Ilovecalculus11>
2021-09-25 12:07:06 -0500

    Added a click method that has random press and release times.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.awt.event.InputEvent;
 import java.awt.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 /*
 
@@ -11,29 +12,36 @@ I am currently a software engineering student at Milwuakee School of Engineering
 I am using this autoclicker as a personal project to better my java skills for the work/intern enviorment.
 
 Feel free to use this auto clicker at your OWN RISK. Auto clickers are against many of the terms of service
-for most online games. It is an unfair advantage and I advise that you try to refrain from cheating.
+for most online games. It is an unfair advantage and I advise that you to try to refrain from cheating.
 
  */
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
+    //Click Method
+    static void click() throws InterruptedException, AWTException {
+        System.out.println("Click");
+        int button = InputEvent.BUTTON1_DOWN_MASK; // Init of click button
+        Robot r = new Robot(); //Init of robot object
+        r.mousePress(button); //Mouse press
+        int pressTime = getRandomInteger(100, 2000);
+        System.out.println(pressTime);
+        Thread.sleep(pressTime); // Pause for random number in milliseconds (pressTime)
+        r.mouseRelease(button); // Release click button
+        int releaseTime = getRandomInteger(100,400);
+        System.out.println(releaseTime);
+        Thread.sleep(releaseTime); // Pause for random number in milliseconds (releaseTime)
+    }
 
-            while(true) {
-                Robot r = new Robot();
-                int button = InputEvent.BUTTON1_DOWN_MASK;
-                System.out.println("Click");
-                r.mousePress(button);
-                Thread.sleep(400);
-                r.mouseRelease(button);
-                Thread.sleep(400);
-            }
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        click();
 
-        } catch (AWTException | InterruptedException e) {
-            e.printStackTrace();
-        }
 
     }
+
+    public static int getRandomInteger(int maximum, int minimum) {
+        return ((int) (Math.random() * (maximum - minimum))) + minimum;
+    }
+
 }
